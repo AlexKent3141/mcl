31f3b258e5923af7f845d3a6639d8ca91fd8498c
Alex Kent <alex.kent1@hotmail.com>
2023-10-08 21:00:38 +0100

    Added basic balanced binary tree implementation with test harness.
    I've gone for a simple AA tree, which offers the standard complexity
    guarantees (O(log(N) insertion and deletion).

---
+++ include/mcl/mcl_bt.h
@@ -0,0 +1,135 @@
+#ifndef __MCL_BT_INCLUDED__
+#define __MCL_BT_INCLUDED__
+
+#include "assert.h"
+#include "stdlib.h"
+
+#define MIN(x, y) (x < y ? x : y)
+
+#define MCL_BT(T, cmp) \
+struct mcl_bt_##T \
+{ \
+  struct mcl_bt_##T* left; \
+  struct mcl_bt_##T* right; \
+  T val; \
+  int level; \
+}; \
+\
+struct mcl_bt_##T* mcl_bt_split_##T(struct mcl_bt_##T* t) \
+{ \
+  if (t == NULL) return t; \
+  if (t->right == NULL || t->right->right == NULL) return t; \
+  if (t->level == t->right->right->level) \
+  { \
+    struct mcl_bt_##T* r = t->right; \
+    t->right = r->left; \
+    r->left = t; \
+    r->level++; \
+    return r; \
+  } \
+  return t; \
+} \
+\
+struct mcl_bt_##T* mcl_bt_skew_##T(struct mcl_bt_##T* t) \
+{ \
+  if (t == NULL) return t; \
+  if (t->left == NULL) return t; \
+  if (t->left->level == t->level) \
+  { \
+    struct mcl_bt_##T* l = t->left; \
+    t->left = l->right; \
+    l->right = t; \
+    return l; \
+  } \
+  return t; \
+} \
+\
+struct mcl_bt_##T* mcl_bt_insert_##T(struct mcl_bt_##T* n, T val) \
+{ \
+  if (!n) \
+  { \
+    struct mcl_bt_##T* n = malloc(sizeof(struct mcl_bt_##T)); \
+    n->left = NULL; \
+    n->right = NULL; \
+    n->val = val; \
+    n->level = 1; \
+    return n; \
+  } \
+  int c = cmp(&val, &n->val); \
+  if (c < 0) n->left = mcl_bt_insert_##T(n->left, val); \
+  else if (c > 0) n->right = mcl_bt_insert_##T(n->right, val); \
+  else return n; \
+  n = mcl_bt_skew_##T(n); \
+  n = mcl_bt_split_##T(n); \
+  return n; \
+} \
+\
+struct mcl_bt_##T* mcl_bt_decrease_level_##T(struct mcl_bt_##T* n) \
+{ \
+  int should_be = n->left && n->right ? MIN(n->left->level, n->right->level) + 1 : 1; \
+  if (should_be < n->level) \
+  { \
+    n->level = should_be; \
+    if (n->right && should_be < n->right->level) n->right->level = should_be; \
+  } \
+\
+  return n; \
+} \
+\
+struct mcl_bt_##T* mcl_bt_predecessor_##T(struct mcl_bt_##T* n) \
+{ \
+  assert(n->left); \
+  n = n->left; \
+  while (n->right) n = n->right; \
+  return n; \
+} \
+\
+struct mcl_bt_##T* mcl_bt_successor_##T(struct mcl_bt_##T* n) \
+{ \
+  assert(n->right); \
+  n = n->right; \
+  while (n->left) n = n->left; \
+  return n; \
+} \
+\
+struct mcl_bt_##T* mcl_bt_delete_##T(struct mcl_bt_##T* n, T val) \
+{ \
+  if (!n) return n; \
+  int c = cmp(&val, &n->val); \
+  if (c < 0) n->left = mcl_bt_delete_##T(n->left, val); \
+  else if (c > 0) n->right = mcl_bt_delete_##T(n->right, val); \
+  else \
+  { \
+    if (n->left) \
+    { \
+      struct mcl_bt_##T* l = mcl_bt_predecessor_##T(n); \
+      n->left = mcl_bt_delete_##T(n->left, l->val); \
+      n->val = l->val; \
+    } \
+    else if (n->right) \
+    { \
+      struct mcl_bt_##T* l = mcl_bt_successor_##T(n); \
+      n->right = mcl_bt_delete_##T(n->right, l->val); \
+      n->val = l->val; \
+    } \
+    else return n->right; \
+  } \
+  n = mcl_bt_decrease_level_##T(n); \
+  n = mcl_bt_skew_##T(n); \
+  n->right = mcl_bt_skew_##T(n->right); \
+  if (n->right) n->right->right = mcl_bt_skew_##T(n->right->right); \
+  n = mcl_bt_split_##T(n); \
+  n->right = mcl_bt_split_##T(n->right); \
+  return n; \
+} \
+\
+int mcl_bt_contains_##T(struct mcl_bt_##T* const n, T val) \
+{ \
+  if (n == NULL) return 0; \
+  int c = cmp(&val, &n->val); \
+  if (c == 0) return 1; \
+  if (c < 0) return mcl_bt_contains_##T(n->left, val); \
+  return mcl_bt_contains_##T(n->right, val); \
+}
+
+#endif /* __MCL_BT_INCLUDED__ */
---
+++ test/bt_test.c
@@ -0,0 +1,65 @@
+#include "mcl/mcl_bt.h"
+#include "mcl/mcl_da.h"
+
+#include "stdio.h"
+
+#define ASSERT(x) \
+{ \
+  if (!(x)) \
+  { \
+    printf("Failed on line %d: %s\n", __LINE__, #x); \
+    return EXIT_FAILURE; \
+  } \
+}
+
+#define TEST_TARGET 1e6
+
+typedef struct _data
+{
+  int val1;
+  float val2;
+} data;
+
+int data_cmp(const data* d1, const data* d2)
+{
+  if (d1->val1 != d2->val1) return d1->val1 < d2->val1 ? -1 : 1;
+  if (d1->val2 != d2->val2) return d1->val2 < d2->val2 ? -1 : 1;
+  return 0;
+}
+
+MCL_DA(data)
+MCL_BT(data, data_cmp)
+
+int main()
+{
+  struct mcl_bt_data* bt = NULL;
+  struct mcl_da_data* da = mcl_da_create_data();
+  int n;
+
+  /* First create all of the data we're interested in. */
+  for (n = 0; n < TEST_TARGET; n++)
+  {
+    data d = { n, (float)n / 2 };
+    mcl_da_push_back_data(da, d);
+  }
+
+  /* Insert the data into a binary tree. */
+  for (n = 0; n < TEST_TARGET; n++)
+  {
+    bt = mcl_bt_insert_data(bt, da->data[n]);
+  }
+
+  /* Check that all elements are present. */
+  for (n = 0; n < TEST_TARGET; n++)
+  {
+    ASSERT(mcl_bt_contains_data(bt, da->data[n]));
+  }
+
+  for (n = 0; n < TEST_TARGET; n++)
+  {
+    bt = mcl_bt_delete_data(bt, da->data[n]);
+    ASSERT(!mcl_bt_contains_data(bt, da->data[n]));
+  }
+
+  return EXIT_SUCCESS;
+}
